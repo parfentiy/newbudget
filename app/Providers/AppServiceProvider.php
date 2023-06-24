1e03a7501220e7f7749dc0dc3d824ac3c6af1b27
parfentiy <parfentiyp@gmail.com>
2023-06-24 19:49:19 +0000

    db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(191);
+	    Schema::defaultStringLength(191);
+
+
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
