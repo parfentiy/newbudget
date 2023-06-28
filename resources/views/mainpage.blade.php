edffce24304cfb2b52d4ff7bff3fc2ae115c28a9
parfentiy <parfentiyp@gmail.com>
2023-06-28 23:21:27 +0300

    mainpage 2

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -5,37 +5,34 @@
         Главная страница
     </div>
     <div class="d-flex flex-row justify-content-center">
-        <div>
+        <div class="d-flex flex-column mx-2 my-2 align-items-center">
             <div>
-                <form name="expense" id="expense" method="post" enctype="multipart/form-data" action="{{url('/add/expense')}}">
-                    @csrf
-                    <input id="go" type="submit"
-                           style="text-indent:-9999px;line-height:0;border:0;background:url('images/newexpense.jpg');height:150px;width:150px;cursor:pointer;float:left;"/>
-                </form>
+                <a href="#"> 
+                    <img src="images/newexpense.jpg"
+                        style="text-indent:-9999px;line-height:0;border:0;height:100px;width:100px;cursor:pointer;float:left;">
+                </a>
             </div>
-            <div style="margin: 0px;">
+            <div>
                 <h2>Добавить расход</h2>
             </div>
         </div>
-        <div>
+        <div class="d-flex flex-column mx-2 my-2 align-items-center">
             <div>
-                <form name="expense" id="expense" method="post" enctype="multipart/form-data" action="{{url('/add/income')}}">
-                    @csrf
-                    <input id="go" type="submit"
-                           style="text-indent:-9999px;line-height:0;border:0;background:url('images/newincome.png');height:150px;width:150px;cursor:pointer;float:left;"/>
-                </form>
+                <a href="#">
+                    <img src="images/newincome.png"
+                        style="text-indent:-9999px;line-height:0;border:0;height:100px;width:100px;cursor:pointer;float:left;"/>
+               </a>
             </div>
             <div>
                 <h2>Добавить доход</h2>
             </div>
         </div>
-        <div>
+        <div class="d-flex flex-column mx-2 my-2 align-items-center">
             <div>
-                <form name="expense" id="expense" method="post" enctype="multipart/form-data" action="{{url('/planning')}}">
-                    @csrf
-                    <input id="go" type="submit"
-                           style="text-indent:-9999px;line-height:0;border:0;background:url('images/planbudget.jpg');height:150px;width:150px;cursor:pointer;float:left;"/>
-                </form>
+                <a href="#">
+                    <img src="images/planbudget.jpg"
+                        style="text-indent:-9999px;line-height:0;border:0;height:100px;width:100px;cursor:pointer;float:center;"/>
+                </a>
             </div>
             <div>
                 <h2>Планирование бюджета</h2>
