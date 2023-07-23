671281fa11ca4c525f0a146e18dcc012da3180ac
parfentiy <parfentiyp@gmail.com>
2023-07-24 01:51:55 +0300

    Поправил версточку в админке

--- app/Http/Middleware/IsUserBanned.php
+++ app/Http/Middleware/IsUserBanned.php
@@ -18,14 +18,13 @@ class IsUserBanned
     public function handle($request, Closure $next)
     {
         if (auth()->check() && auth()->user()->banned_till != null) {
-
-            if (auth()->user()->banned_till == 0) {
-                $message = 'Your account has been banned permanently.';
+            if (auth()->user()->banned_till === '0') {
+                $message = 'Ваш логин временно заблокирован. Обратитесь к администратору сайта.';
             }
-            if (now()->lessThan(auth()->user()->banned_till)) {
+            /*if (now()->lessThan(auth()->user()->banned_till)) {
                 $banned_days = now()->diffInDays(auth()->user()->banned_till) + 1;
                 $message = 'Your account has been suspended for ' . $banned_days . ' ' . Str::plural('day', $banned_days);
-            }
+            }*/
 
             auth()->logout();
             return redirect()->route('login')->with('message', $message);
