1461df57f6b29f0a20bb802fcb4fbcae8b2a4b4a
parfentiy <parfentiyp@gmail.com>
2023-07-25 00:15:42 +0300

    фикс2

--- config/telegram.php
+++ config/telegram.php
@@ -32,9 +32,9 @@ return [
     */
     'bots' => [
         'mybot' => [
-            'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
-            'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
+            'token' => env('TELEGRAM_BOT_TOKEN', ''),
+            //'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', ''),
+            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', ''),
             'commands' => [
                 //Acme\Project\Commands\MyTelegramBot\BotCommand::class
             ],
